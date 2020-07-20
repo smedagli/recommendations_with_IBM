672d661dc934082c8ea487c220306f05f4abcca9
smedagli <stefano.medagli@rohde-schwarz.com>
2020-07-20 15:02:41 +0200

    feat: add general information of the project in 'info'

---
+++ info/paths.py
@@ -0,0 +1,8 @@
+"""
+This module defines the default paths of the project
+"""
+import os
+
+
+articles_path = os.path.join('data', 'articles_community.csv')
+users_path = os.path.join('data', 'user-item-interactions.csv')
---
+++ info/version.py
@@ -0,0 +1,3 @@
+
+__version__ = 0.1
+__text_version__ = f'v{"_".join(str(__version__).split("."))}'
